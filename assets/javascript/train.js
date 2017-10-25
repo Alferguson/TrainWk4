d90c7117804d2fddbacee1d42d84a567a082a2e9
Alferguson <alferguson@berkeley.edu>
2017-10-24 18:56:03 -0700

    almost done but errors

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,12 +1,16 @@
 
 var config = {
-  apiKey: "AIzaSyAJS4YQWU5DmESeYueG1qH1NGkjv3DncEY",
-  authDomain: "fir-click-counter-7cdb9.firebaseapp.com",
-  databaseURL: "https://fir-click-counter-7cdb9.firebaseio.com",
-  storageBucket: "fir-click-counter-7cdb9.appspot.com"
-};
+    apiKey: "AIzaSyBpibu-G0Uoyl7gSPIL9JrdfS3IOAcmisQ",
+    authDomain: "trainwk4-12c7c.firebaseapp.com",
+    databaseURL: "https://trainwk4-12c7c.firebaseio.com",
+    projectId: "trainwk4-12c7c",
+    storageBucket: "trainwk4-12c7c.appspot.com",
+    messagingSenderId: "897917821206"
+  };
 firebase.initializeApp(config);
+
 var database = firebase.database();
+
 var trainForm;
 var destinationForm;
 var firstTrainTimeForm;
@@ -15,14 +19,31 @@ var nextArrival;
 var minutesAway;
 var currentTime;
 var tableTd;
-var trainTd;
-var destinationTd;
-var frequencyTd;
-var nextArrivalTd;
-var minutesAwayTd;
 var tableTr;
-var tableTbody;
+var hourFormat;
 
+// checks current time with first train time with moment.js
+var timeCheck = function() {
+	var hourFormat = "h:mm";
+	var convertedFirstTrainTimeForm = moment(firstTrainTimeForm).format(hourFormat);
+	var convertedFrequencyForm = moment(frequencyForm).format("mm");
+	var currentTime = moment().format(hourFormat);
+
+	if (currentTime < convertedFirstTrainTimeForm) {
+		var nextArrival = convertedFirstTrainTimeForm;
+	}
+	else if (currentTime == convertedFirstTrainTimeForm) {
+		var nextArrival = "Train is here";
+	}
+	else if (currentTime > convertedFirstTrainTimeForm) {
+		while (currentTime > convertedFirstTrainTimeForm) {
+			convertedFirstTrainTimeForm.add(convertedFrequencyForm, "minutes").calendar
+			var nextArrival = convertedFirstTrainTimeForm;
+		}
+	}
+	var minutesAway = currentTime.diff(nextArrival, "minutes");
+
+};
 
 // gets user forms and gives them to firebase
 var formToFirebase = function() {
@@ -32,66 +53,45 @@ var formToFirebase = function() {
 	destinationForm = $("#destination-form").val().trim();
 	firstTrainTimeForm = $("#first-train-time-form").val().trim();
 	frequencyForm = $("#frequency-form").val().trim();
-
+	timeCheck();
 	// Change what is saved in firebase
-    database.ref().set({
+    database.ref().push ({
     	trainForm: trainForm,
         destinationForm: destinationForm,
-        firstTrainTimeForm: firstTrainTimeForm,
-        frequencyForm: frequencyForm
+        frequencyForm: frequencyForm,
+        nextArrival: nextArrival,
+        minutesAway: minutesAway
     });
-}
-// checks current time with first train time
-var timeCheck = function() {
-	if (currentTime < firstTrainTimeForm) {
-		var nextArrival = firstTrainTimeForm;
-	}
-	else if (currentTime == firstTrainTimeForm) {
-		var nextArrival = "Train is here";
-	}
-	else if (currentTime > firstTrainTimeForm) {
-		for (currentTime > firstTrainTimeForm) {
-			firstTrainTimeForm = firstTrainTimeForm + frequencyForm;
-			var nextArrival = firstTrainTimeForm;
-		}
-	}
-	// display nextArrival
-	var minutesAway = nextArrival - currentTime;
-	// display minutesAway
-}
+};
+
 // dynamically creates tables
-var tableCreator = function() {
-	tableTd = $("<td>");
-	trainTd = tableTd.text(trainForm);
-	destinationTd = tableTd.text(destinationForm);
-	frequencyTd = tableTd.text(frequencyForm);
-	nextArrivalTd = tableTd.text(timeCheck());
-	minutesAwayTd = tableTd.text(timeCheck());
+var tableCreator = function(childSnapshot) {
+	trainForm = childSnapshot.val().trainForm;
+    destinationForm = childSnapshot.val().destinationForm;
+    frequencyForm = childSnapshot.val().frequencyForm;
+    nextArrival = childSnapshot.val().nextArrival;
+    minutesAway = childSnapshot.val().minutesAway;
+
+    
+	
 	tableTr = $("<tr>");
-	tableTbody = $("<tbody>");
-	tableTbody.append(tableTr.append(destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd));
+	tableTr.append("<td>" + trainForm + "</td>");
+    tableTr.append("<td>" + destinationForm + "</td>");
+    tableTr.append("<td>" + frequencyForm + "</td>");
+    tableTr.append("<td>" + monthsWorked + "</td>");
+    tableTr.append("<td>" + nextArrival + "</td>");
+    tableTr.append("<td>" + minutesAway + "</td>");
+	
+	
+	$("#train-table").append(tableTr);
 
-}
-
-// When changes occurs it will print them to console and html
-database.ref().on("value", function(snapshot) {
-	 // Print the initial data to the console.
-      console.log(snapshot.val());
-
-      // Log the value user data
-      console.log(snapshot.val().trainForm);
-      console.log(snapshot.val().destinationForm);
-      console.log(snapshot.val().firstTrainTimeForm);
-      console.log(snapshot.val().frequencyForm);
-
-      // Change html of train-table id to dynamically created table points
-      $("#train-table").append(snapshot.val().name + " | " + snapshot.val().age + " | " + snapshot.val().phone);
-
-}, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
-
-
-
+}, 
+// function(errorObject) {
+//       console.log("The read failed: " + errorObject.code);
+// }
 
 // take text in forms and sends it to firebase
-$("#submit").on("click", formToFirebase());
+$("#submit").on("click", formToFirebase);
+
+// when database has new values, create tables accordingly
+database.ref().on("child_added", tableCreator);
