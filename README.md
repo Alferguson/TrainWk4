b61831dfd3ff7b9882f4214bf1391203c9a77a91
Alferguson <alferguson@berkeley.edu>
2017-10-26 20:47:51 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TrainWk4
+
+
+Hey, I am entering changes here for the homework github thing
--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,4 +1,4 @@
-
+// to initialize firebase
 var config = {
     apiKey: "AIzaSyBpibu-G0Uoyl7gSPIL9JrdfS3IOAcmisQ",
     authDomain: "trainwk4-12c7c.firebaseapp.com",
@@ -10,19 +10,19 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
+// variable declarations
 var trainForm;
 var destinationForm;
 var firstTrainTimeForm;
+var convertedFirstTrainTimeForm;
 var frequencyForm;
+var timeDifferent;
 var nextArrival;
 var minutesAway;
+var remainder;
 var currentTime;
 var tableTd;
 var tableTr;
-
-
-
 
 // gets user forms and gives them to firebase
 $("#submit").on("click", function() {
@@ -37,22 +37,19 @@ $("#submit").on("click", function() {
 	
 	var convertedFirstTrainTimeForm = moment(firstTrainTimeForm, "HH:mm");
 	
-	
-	
 	var timeDifferent = moment().diff(moment(convertedFirstTrainTimeForm), "minutes");
 	// if loop to determine if the first train arrival is after the current time
 	if (timeDifferent > 0) {
 		var remainder = timeDifferent % frequencyForm;
 		var minutesAway = frequencyForm - remainder;
-		debugger;
 		var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
-		debugger;
 	}
+	// if first arrival is after current time, just used converted first train time for next arrival
 	else {
 		nextArrival = convertedFirstTrainTimeForm.format("HH:mm");
 		minutesAway = timeDifferent * -1;
 	}	
-
+	// push data to firebase
     database.ref().push ({
     	trainFormRef: trainForm,
         destinationFormRef: destinationForm,
@@ -60,23 +57,19 @@ $("#submit").on("click", function() {
         nextArrivalRef: nextArrival,
         minutesAwayRef: minutesAway
     });
-    // var postId = database.ref().push().key;
 });
 
-
+// update function every minute, doesn't work
 var update = function () {
+	// set current time and convert user inputs to hours and minutes
 	var currentTime = moment();
-
 	var convertedFirstTrainTimeForm = moment(firstTrainTimeForm, "HH:mm");
-
 	var timeDifferent = moment().diff(moment(convertedFirstTrainTimeForm), "minutes");
 	// if loop to determine if the first train arrival is after the current time
 	if (timeDifferent > 0) {
 		var remainder = timeDifferent % frequencyForm;
 		var minutesAway = frequencyForm - remainder;
-		debugger;
 		var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
-		debugger;
 	}
 	else {
 		nextArrival = convertedFirstTrainTimeForm.format("HH:mm");
@@ -86,22 +79,18 @@ var update = function () {
 		nextArrivalRef: nextArrival,
 		minutesAwayRef: minutesAway
 	})	
-	
 }
-
 
 // dynamically creates tables
 database.ref().on("child_added", function(childSnapshot) {
-	debugger;
-
+	// set variables to what firebase has
 	trainForm = childSnapshot.val().trainFormRef;
     destinationForm = childSnapshot.val().destinationFormRef;
     frequencyForm = childSnapshot.val().frequencyFormRef;
     nextArrival = childSnapshot.val().nextArrivalRef;
     minutesAway = childSnapshot.val().minutesAwayRef;
-    keyId = childSnapshot.key;
     
-  
+
 	// to make a new row for when data is submitted
 	tableTr = $("<tr>");
 	tableTr.append("<td>" + trainForm + "</td>");
@@ -110,31 +99,81 @@ database.ref().on("child_added", function(childSnapshot) {
     tableTr.append("<td>" + nextArrival + "</td>");
     tableTr.append("<td>" + minutesAway + "</td>");
 	
-	
+	// append to html id train table
 	$("#train-table").append(tableTr);
-
-	timerFunc = setInterval(update, 60000);
-
+	setInterval(timeUpdater, 1000);
+	// timer function to update data, doesn't work
+	
 }), 
 function(errorObject) {
       console.log("The read failed: " + errorObject.code);
 }
 
-database.ref().on("child_changed", function(childSnapshot) {
-	$("#train-table").empty();
-
-	// to make a new row for when data is submitted
-	tableTr = $("<tr>");
-	tableTr.append("<td>" + trainForm + "</td>");
-    tableTr.append("<td>" + destinationForm + "</td>");
-    tableTr.append("<td>" + frequencyForm + "</td>");
-    tableTr.append("<td>" + nextArrival + "</td>");
-    tableTr.append("<td>" + minutesAway + "</td>");
+// function newUpdate() {
 	
+// }
+
+
+// to upate tables every minute 
+// database.ref().on("value", function(childSnapshot) {
+// 	setInterval(newUpdate, 60000);
+// }) 
+
+
+// Update minutes away by triggering change in firebase children
+    function timeUpdater() {
+    	debugger;
+    var currentTime = moment();
+	var convertedFirstTrainTimeForm = moment(firstTrainTimeForm, "HH:mm");
+	var timeDifferent = moment().diff(moment(convertedFirstTrainTimeForm), "minutes");
+	// if loop to determine if the first train arrival is after the current time
+	if (timeDifferent > 0) {
+		var remainder = timeDifferent % frequencyForm;
+		var minutesAway = frequencyForm - remainder;
+		var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
+	}
+	else {
+		nextArrival = convertedFirstTrainTimeForm.format("HH:mm");
+		minutesAway = timeDifferent * -1;
+	}
+database.ref().once('value', function(snapshot){
 	
-	$("#train-table").append(tableTr);
+        snapshot.forEach(function(childSnapshot){
+          database.ref(childSnapshot).update({
+          minutesAwayRef: minutesAway,
+          nextArrivalRef: nextArrival
+          
+          })
+        })    
+      });
+    };
 
-	timerFunc = setInterval(update, 60000);
+	// // to make a new row for when data is submitted
+	// tableTr = $("<tr>");
+	// tableTr.append("<td>" + trainForm + "</td>");
+ //    tableTr.append("<td>" + destinationForm + "</td>");
+ //    tableTr.append("<td>" + frequencyForm + "</td>");
+ //    tableTr.append("<td>" + nextArrival + "</td>");
+ //    tableTr.append("<td>" + minutesAway + "</td>");
+	
+	// // append to html id train table
+	// $("#train-table").append(tableTr);
+      
 
+    
+// // doesn't really work either
+// database.ref().on("child_changed", function(childSnapshot) {
+// 	$("#train-table").empty();
 
-});
+// 	// to make a new row for when data is submitted
+// 	tableTr = $("<tr>");
+// 	tableTr.append("<td>" + trainForm + "</td>");
+//     tableTr.append("<td>" + destinationForm + "</td>");
+//     tableTr.append("<td>" + frequencyForm + "</td>");
+//     tableTr.append("<td>" + nextArrival + "</td>");
+//     tableTr.append("<td>" + minutesAway + "</td>");
+	
+// 	$("#train-table").append(tableTr);
+
+// 	timerFunc = setInterval(update, 60000);
+// });
